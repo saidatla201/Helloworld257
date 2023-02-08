805ad08ff04ee0bc8ebd6a62484d2a1f0616030d
saidatla201 <saidatla201@gmail.com>
2023-02-08 07:50:20 +0530

    Username

---
+++ username.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    public class username
+    {
+        internal static void Display()
+        {
+            Console.WriteLine("Enter the  usename ");
+
+            string username = Console.ReadLine();
+
+            int val = username.Length;
+
+            Console.WriteLine("The num of characters is " + val);
+            if (username.Length > 3)
+            {
+                Console.WriteLine("hello {0}, How are you!", username);
+
+            }
+            else
+            {
+                Console.WriteLine("The name is not having min 3 characters ");
+            }
+        }
+    }
+}
